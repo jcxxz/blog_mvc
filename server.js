3f3e90fb784d786a38589e0034d5ff3ed280cc22
Joseph Cornelius <josephcornelius02@outlook.com>
2023-05-08 18:04:15 -0700

    somewhat finished code

--- server.js
+++ server.js
@@ -1,15 +1,26 @@
 const express = require("express");
 const session = require("express-session");
 const exphbs = require("express-handlebars");
+const sequelize = require("./config/connection");
+const SequelizeStore = require("connect-session-sequelize")(session.Store);
+require("dotenv").config();
 
 const path = require("path");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-app.engine('handlebars', engine());
+const handlebars = exphbs.create({
+    helpers: {
+        format_date: date => {
+            return `${date.getMonth() + 1}/${date.getDate()}/${date.getFullYear()}`;
+        }
+    }
+});
+
+app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
-app.set('views', './views');
+
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
